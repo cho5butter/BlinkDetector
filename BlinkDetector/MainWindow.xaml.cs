0cad85e22338c0449ee0bbb44d663e8adf0ba6fc
cho5butter <akutore@gmail.com>
2022-06-28 20:59:09 +0900

    add opencv

--- BlinkDetector/MainWindow.xaml.cs
+++ BlinkDetector/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Hardcodet.Wpf.TaskbarNotification;
 using Microsoft.Toolkit.Uwp.Notifications;
+using OpenCvSharp;
 
 namespace BlinkDetector
 {
@@ -21,24 +22,59 @@ namespace BlinkDetector
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+
+    public partial class MainWindow : System.Windows.Window
     {
+        private VideoCapture capture;
+        private Mat frame;
+        private System.Drawing.Bitmap bmp;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Drawing.Graphics graphic;
         public MainWindow()
         {
+
             InitializeComponent();
             this.Visibility = Visibility.Hidden;
             new ToastContentBuilder()
                 .AddText("入力してね！")
                 .AddInputTextBox("textbox", "this is placeholder ...", "手入力欄")
                 .AddComboBox("combobox", "選択欄", "a", new (string, string)[]{
-        ("a", "選択肢A"),
-        ("b", "選択肢B"),
-        ("c", "選択肢C"),
+                    ("a", "選択肢A"),
+                    ("b", "選択肢B"),
+                    ("c", "選択肢C"),
                 })
                 .AddButton(new ToastButton("OK", "ok"))
                 .AddButton(new ToastButton("キャンセル", "cancel"))
                 .Show();
 
+
+
+        }
+
+        private void GetWebcam()
+        {
+            int WIDTH = 640;
+            int HEIGHT = 480;
+
+            capture = new VideoCapture();
+            if (!capture.IsOpened())
+            {
+                MessageBox.Show("camera was not found!");
+                this.Close();
+            }
+            capture.FrameWidth = WIDTH;
+            capture.FrameHeight = HEIGHT;
+
+            frame = new Mat(HEIGHT, WIDTH, MatType.CV_8UC3);
+
+            bmp = new System.Drawing.Bitmap(frame.Cols, frame.Rows, (int)frame.Step(), System.Drawing.Imaging.PixelFormat.Format24bppRgb, frame.Data);
+
+            pictureBox1 = new System.Windows.Forms.PictureBox();
+
+            pictureBox1.Width = frame.Cols;
+            pictureBox1.Height = frame.Rows;
+
+            graphic = pictureBox1.CreateGraphics();
         }
     }
 }
